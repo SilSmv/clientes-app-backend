9ebf6e28a5e5062128a5030b8da252249bfd0fd1
SilSmv <silvana.munoz.vargas@gmail.com>
2022-09-21 12:04:31 -0400

    Backend creation

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
+insert into clientes(nombre,apellido,email,create_at) values ("Silvana","Munoz","sil@gmail.com","2022-01-01");
