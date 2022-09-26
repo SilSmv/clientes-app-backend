b9a6f81c730de2055d4fc60d68d8d40f5243b801
SilSmv <silvana.munoz.vargas@gmail.com>
2022-09-26 19:21:24 -0400

    Add data validation

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -12,6 +12,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 @Entity
 @Table(name="clientes")
 public class Cliente implements Serializable{
@@ -20,14 +23,23 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
+	
+	@NotEmpty
+	@Size(min= 4, max = 12)
 	@Column(nullable=false)
 	private String nombre;
+	
+	@NotEmpty
 	private String apellido;
 	@Column(nullable=false, unique=true)
+	
+	@NotEmpty
+	@Email
 	private String email;
+	
 	@Column(name="create_at")
 	@Temporal(TemporalType.DATE)
-	private Date createAt;
+	private Date createAt; 
 	
 	@PrePersist
 	public void prePersist() {
